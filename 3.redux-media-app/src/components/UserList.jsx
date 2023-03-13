659e84f6efdc458cdeaab0191da7e124d4dca99e
Shivanand <Shivanand0297@gmail.com>
2023-03-13 11:36:01 +0530

    different loading states for creating and loading users

--- 3.redux-media-app/src/components/UserList.jsx
+++ 3.redux-media-app/src/components/UserList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // async thunk for fetching data
@@ -9,46 +9,67 @@ import Skeleton from "./Skeleton";
 import List from "./List";
 
 const UserList = () => {
+  // to keep track of loading users
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUserError, setLoadingUserError] = useState(null);
+
+  // to keep track of creating user state
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
+
   const dispatch = useDispatch();
-  const { data, isLoading, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.user;
   });
 
   useEffect(() => {
+    setIsLoadingUsers(true);
     // dipatching action for making request
-    dispatch(fetchUsers());
+
+    // dispatch(fetchUsers()).then() this will call the then() whether the promise resolves or rejects
+    dispatch(fetchUsers())
+      .unwrap() //this will give the traditional promise back we can use .then() like we use to do
+      .catch((err) => setLoadingUserError(err))
+      .finally(() => setIsLoadingUsers(false));
+
+    // Note: after making request DO NOT MAKE setIsLoadingUsers(false)
+    // setIsLoadingUsers(false) NOPE!!!, because here async thunk dispatch() function is async is nature it will immediately setIsLoadingUsers to false
 
     // eslint-disable-next-line
   }, []);
 
-  const handleAddUser = () =>{
+  const handleAddUser = () => {
+    setIsCreatingUser(true);
+
     dispatch(addUser())
-  }
+      .unwrap()
+      .catch((err) => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
+  };
 
   return (
     <div>
-      {isLoading ? (
-        <Skeleton times={8} size="h-6 w-4/5"/>
+      {isLoadingUsers ? (
+        <Skeleton times={8} size="h-6 w-4/5" />
       ) : (
         <>
-          <div
-            className="flex flex-col justify-start container mx-auto w-4/5 m-2"
-          >
-
+          <div className="flex flex-col justify-start container mx-auto w-4/5 m-2">
             <div className="flex justify-end">
-              <button
-                className="bg-blue-400 border rounded text-white px-2 py-[6px] text-sm"
-                onClick={handleAddUser}
-              >
-                + Add User
-              </button>
+              {isCreatingUser ? (
+                "Creating user..."
+              ) : (
+                <button
+                  className="bg-blue-400 border rounded text-white px-2 py-[6px] text-sm"
+                  onClick={handleAddUser}
+                >
+                  + Add User
+                </button>
+              )}
+              {creatingUserError && "error creating user"}
             </div>
 
-            {data && data.map((user) => (
-              <List key={user.id} user={user} />
-              ))}
-            <p>{error ? "Error Fetching data" : null}</p>
-
+            {data && data.map((user) => <List key={user.id} user={user} />)}
+            <p>{loadingUserError ? "Error Fetching data" : null}</p>
           </div>
         </>
       )}
